4c45d28c43f2b797779012ba83defcea388a8e73
Jorge12as <jejsantosce@gmail.com>
2025-07-02 08:49:21 -0300

    Update calculaIdade.js
    Atualizar função para validar data

--- calculaIdade.js
+++ calculaIdade.js
@@ -174,16 +174,3 @@ function mascaraData(campo) {
 
 
 
-/*function mascaraData(campo, e) {
-  var kC = document.querySelectorAlll ? e.keyCode : e.keyCode;
-  var data = campo.value;
-
-  if (kC != 8 && kC != 46) {
-    if (data.length == 2) {
-      campo.value = data += "/";
-    } else if (data.length == 5) {
-      campo.value = data += "/";
-    } else campo.value = data;
-  }*/
-
-
